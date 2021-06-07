fbdd6b035a25545dfc2972fced7acfcac6dd7b82
Igor Shkatov <66472750+IgorShkatov@users.noreply.github.com>
2021-06-08 00:39:25 +0300

    finish

---
+++ resources/js/components/MainPage.vue
@@ -0,0 +1,31 @@
+<template>
+<div class="container w-50 ">
+<div class="text-center m-5">
+ <div class="col">
+          <div class="card shadow-sm">
+              <div class="text-center">
+  <img src="/img/foto.jpeg " class="rounded w-50 h-50 mt-3" alt="...">
+</div>
+            <div class="card-body">
+              <p class="card-text">Привет!Меня зовут Игорь. Я из Москвы мне 26 лет. Я работал в продажах с 2012 года, но в 2019 все круто поменялось...Я решил уйти в быстроразвивающуюся и интересную для меня сферу - это IT. Я начал с изучения основ web-разработки это: HTML,CSS,JS. Все давалось довольно легко, были заминки с JS но в итоге я со всем разобрался. Позже пошел на курсы web-разработки, где под руководсвом ментора освоил такие библиотеки как React,JQuery познакомился с PHP,MySQL, сделал несколько учебных проектов как на фронте так и на бэке. Сейчас нахожусь в активном поиске работы, готов учиться новому и развиваться в данной сфере!)</p>
+              <div class="d-flex justify-content-between align-items-center">
+               
+              
+              </div>
+            </div>
+          </div>
+        </div>
+ 
+</div>
+</div>
+
+
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
